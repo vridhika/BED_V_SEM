028bc1d08559cf07a1610dbc0b6ed53c8e73d8dc
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2025-07-10 15:49:26 +0530

    bee

---
+++ Lec-2/file1.js
@@ -0,0 +1,11 @@
+let file2=require("./file2");
+console.log(file2);
+let res=mul(2,3)
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+module.exports.add=add;
+module.exports.sub=sub;
